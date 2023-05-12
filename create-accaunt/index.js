20675d78beed729596bd421eaa50a09a4481c4c6
unknown <robakidzenino89@gmail.com>
2023-05-12 19:30:59 +0400

    workwise

---
+++ create-accaunt/index.js
@@ -0,0 +1,7 @@
+const radio = document.getElementById("myradio1");
+const label = document.querySelector(".radio_lab");
+
+myradio1.onclick = function (){
+    radio_lab.style.Color = "black";
+
+};
